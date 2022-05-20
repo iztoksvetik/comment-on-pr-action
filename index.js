efffce8a45a4be2c2a87eb3013d2a54e628d0efa
Iztok Svetik <iztoksvetik@users.noreply.github.com>
2022-05-20 18:26:04 +0200

    Implement

--- index.js
+++ index.js
@@ -2,16 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    // const token = core.getInput('github-token');
-    // const client = github.getOctokit(token);
-    // const response = client.rest.issues.createComment({
-    //     ...github.context.repo,
-    //     issue_number: github.context.issue.number,
-    //     body: '👋 Hello!',
-    // });
-
     run().then(_ => core.info("Successfully ran"));
-
 } catch (error) {
     core.setFailed(error.message);
 }
@@ -22,10 +13,29 @@ async function run() {
     const existingComment = await get_existing_comment(client);
     if (existingComment) {
         core.info("Comment ID: " + existingComment.id);
+        const response = await update_comment(client, existingComment.id);
+        core.info(JSON.stringify(response, null, 2));
     } else {
-        core.info("No comment found");
+        core.info("No comment found, creating new one");
+        const response = await create_comment(client);
+        core.info(JSON.stringify(response, null, 2));
     }
+}
 
+async function create_comment(client) {
+    return await client.rest.issues.createComment({
+        ...github.context.repo,
+        issue_number: github.context.issue.number,
+        body: '👋 New comment, wow!',
+    });
+}
+
+async function update_comment(client, commentId) {
+    return await client.rest.issues.updateComment({
+        ...github.context.repo,
+        comment_id: commentId,
+        body: "Updated comment 🚀"
+    });
 }
 
 async function get_existing_comment(client) {
