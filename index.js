920d1923bbd2ebe991791e772d35b592f9620f41
Iztok Svetik <iztoksvetik@users.noreply.github.com>
2022-05-21 00:08:05 +0200

    Implement core functionality (#1)

--- index.js
+++ index.js
@@ -1,16 +1,64 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const repo = _ => {
+    const [owner, repo] = core.getInput('repo').split("/");
+    return { 'owner': owner, 'repo': repo };
+};
+
 try {
-    const token = core.getInput('github-token');
-    const client = github.getOctokit(token);
-
-    const response = await client.rest.issues.createComment({
-        ...github.context.issue,
-        body: '👋 Hello!',
+    run().then(commentId => {
+        core.info(`Comment ID: ${commentId}`);
+        core.setOutput('comment-id', commentId);
     });
-
-    console.log(response);
 } catch (error) {
     core.setFailed(error.message);
+}
+
+async function run() {
+    const issue = core.getInput('issue');
+    if (!issue) {
+        throw Error('No issue number found. Outside of PRs and Issues set issue nr. as input parameter.');
+    }
+    const token = core.getInput('github-token');
+    const update = core.getInput('update-comment') === 'true';
+    const client = github.getOctokit(token);
+
+    if (update) {
+        const existingComment = await get_existing_comment(client);
+        if (existingComment) {
+            core.info("Updating comment: " + existingComment.id);
+            const {data: comment} = await update_comment(client, existingComment.id);
+            return comment.id;
+        }
+    }
+
+    core.info("Creating a new comment");
+    const {data: comment} = await create_comment(client, issue);
+    return comment.id;
+}
+
+async function create_comment(client, issue) {
+    return await client.rest.issues.createComment({
+        ...repo(),
+        issue_number: issue,
+        body: core.getInput('content'),
+    });
+}
+
+async function update_comment(client, commentId) {
+    return await client.rest.issues.updateComment({
+        ...repo(),
+        comment_id: commentId,
+        body: core.getInput('content')
+    });
+}
+
+async function get_existing_comment(client) {
+    const {data: comments} = await client.rest.issues.listComments({
+        ...github.context.repo,
+        issue_number: github.context.issue.number
+    });
+    core.info("Get existing comments")
+    return comments.find(element => element.user.login === core.getInput('username'))
 }
