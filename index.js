f6367567b0a94413e80fa6a94867cffa04ba054c
Iztok Svetik <iztoksvetik@users.noreply.github.com>
2022-05-20 16:29:12 +0200

    Test github api

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ const github = require('@actions/github');
 try {
     const token = core.getInput('github-token');
     const client = github.getOctokit(token);
-    const response = client.rest.issues.createComment({
-        ...github.context.repo,
-        issue_number: github.context.issue.number,
-        body: '👋 Hello!',
-    });
+    // const response = client.rest.issues.createComment({
+    //     ...github.context.repo,
+    //     issue_number: github.context.issue.number,
+    //     body: '👋 Hello!',
+    // });
 
-    console.log(response);
+    const response = client.rest.issues.listComments({
+        ...github.context.repo,
+        issue_number: github.context.issue.number
+    }).then(response => console.log(response));
+
+    console.log(github.context.actor);
+
+    client.rest.users.getAuthenticated().then(response => console.log(response));
+
 } catch (error) {
     core.setFailed(error.message);
 }
